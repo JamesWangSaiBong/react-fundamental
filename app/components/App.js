6ede9a8e55a36fe4c49b7d0298ada1a6783d9938
James Wang <jwsb0702@gmail.com>
2017-05-27 22:27:27 -0400

    Implemented React Router. Created Home and Battle page

--- app/components/App.js
+++ app/components/App.js
@@ -1,15 +1,31 @@
 'use strict';
 
-var Badge = require('./Badge');
+var Nav = require('./Nav');
+var Home = require('./Home');
+var Battle = require('./Battle');
 var Popular = require('./Popular');
 var React = require('react');
+var ReactRouter = require('react-router-dom');
+var Router = ReactRouter.BrowserRouter;
+var Route = ReactRouter.Route;
+var Switch = ReactRouter.Switch;
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <Popular />
-      </div>
+      <Router>
+        <div className='container'>
+          <Nav />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/battle' component={Battle} />
+            <Route path='/popular' component={Popular} />
+            <Route render={function() {
+              return <p>Not Found</p>
+            }} /> 
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
---
+++ app/components/Battle.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var React = require('react');
+
+class Battle extends React.Component {
+  render() {
+    return (<h1>Battle!!</h1>);
+  }
+}
+
+module.exports = Battle;
