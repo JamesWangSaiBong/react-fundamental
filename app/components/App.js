7bd23909b28c432521d65f2dca0703a6664b08ef
James Wang <jwsb0702@gmail.com>
2017-05-27 20:52:38 -0400

    Created api methods to fetch repos. Created RepoGrid component

--- app/components/App.js
+++ app/components/App.js
@@ -9,11 +9,6 @@ class App extends React.Component {
     return (
       <div>
         <Popular />
-        <Badge 
-          img='https://avatars0.githubusercontent.com/u/2933430?v=3&s=460'
-          name='Tyler McGinnis'
-          username='tylermcginnis'
-        />
       </div>
     );
   }
