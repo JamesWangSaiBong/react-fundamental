2b1a1280c093ee7ccf825deb3d62b329ecc77873
James Wang <jwsb0702@gmail.com>
2017-05-28 16:37:04 -0400

    Completed Battle page

--- app/components/App.js
+++ app/components/App.js
@@ -4,6 +4,7 @@ var Nav = require('./Nav');
 var Home = require('./Home');
 var Battle = require('./Battle');
 var Popular = require('./Popular');
+var Results = require('./Results');
 var React = require('react');
 var ReactRouter = require('react-router-dom');
 var Router = ReactRouter.BrowserRouter;
@@ -19,6 +20,7 @@ class App extends React.Component {
           <Switch>
             <Route exact path='/' component={Home} />
             <Route exact path='/battle' component={Battle} />
+            <Route path='/battle/results' component={Results} />
             <Route path='/popular' component={Popular} />
             <Route render={function() {
               return <p>Not Found</p>
---
+++ app/components/Results.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var React = require('react');
+
+class Results extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Results</h1>
+      </div>
+    );
+  }
+}
+
+module.exports = Results;
