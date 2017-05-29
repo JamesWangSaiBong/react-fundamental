6ebfe6a9390973bffc75ab9e6a44690c72e14870
James Wang <jwsb0702@gmail.com>
2017-05-28 22:14:23 -0400

    Forget about the previous commit

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,6 +1,6 @@
 var axios = require('axios');
 
-var accessToken = 'ACCESS_TOKEN';
+var accessToken = '817d0f6eb5e1440cc082ccfa6fd66581e1aa565a';
 var params = `?access_token=${accessToken}`;
 
 function getProfile (username) {
